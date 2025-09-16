35817c11886cccd4eb46bc00f2eec59b60247413
KanittaJHA <kanittaketin@gmail.com>
2025-09-17 02:32:12 +0700

    modified middlewares/csrfMiddleware.js

--- middlewares/csrfMiddleware.js
+++ middlewares/csrfMiddleware.js
@@ -4,17 +4,11 @@ export const verifyCsrf = (req, res, next) => {
   const csrfTokenHeader = req.headers["x-csrf-token"];
   const csrfTokenCookie = req.cookies["csrfToken"];
 
-  console.log("=== CSRF Check ===");
-  console.log("CSRF Cookie:", csrfTokenCookie);
-  console.log("CSRF Header:", csrfTokenHeader);
-
-  if (!csrfTokenHeader || !csrfTokenCookie) {
-    console.warn("CSRF token missing in request");
-    return next(new ApiError(403, "CSRF token missing"));
-  }
-
-  if (csrfTokenHeader !== csrfTokenCookie) {
-    console.warn("CSRF token mismatch");
+  if (
+    !csrfTokenHeader ||
+    !csrfTokenCookie ||
+    csrfTokenHeader !== csrfTokenCookie
+  ) {
     return next(new ApiError(403, "Invalid CSRF token"));
   }
 
